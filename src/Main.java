c2215713267a9d4b6daaef3e27c9af38eeb2ed9f
Krygant <krygant8484@gmail.com>
2025-02-03 22:05:15 +0300

    Demo_main 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int i = 5;
+        int ii = 0;
+        i += i;
+        ii += i;
         System.out.println("Hello world!");
     }
     //коммент
